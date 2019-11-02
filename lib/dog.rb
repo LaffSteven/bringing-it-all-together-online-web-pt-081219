fe09701b0e6121df9627596919bf2020f6b45936
Steven Laff <steve.laff@gmail.com>
2019-11-02 15:52:37 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,7 +3,10 @@ class Dog
   attr_accessor :id, :name, :breed
 
   def initialize(attributes)
-    attributes.each {|key, value| self.send(("#{key}="), value)}
+    attributes.each do |key, value| 
+      self.send(("#{key}="), value)
+    end
+    
     self.id ||= nil
   end
 
